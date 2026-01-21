8e19d0ed091a450376713fdd4d282537e1fb5bb0
mico <wilder@micous.com>
2026-01-21 11:40:34 +0800

    update: config file changes

--- app/config.py
+++ app/config.py
@@ -47,7 +47,7 @@ class Settings(BaseSettings):
 
     # Stripe Config
     STRIPE_SECRET_KEY: str = "sk_test_51SreWwGVP93aj81To6Xp9DreJPPieqnIfmDhBAQkAJFjdDCTNfsvUT6JVAC4t5dEYsw6jPaajFNJQDomcH6Q6YN200XwCuruCx"
-    STRIPE_WEBHOOK_SECRET: str = ""
+    STRIPE_WEBHOOK_SECRET: str = "whsec_NUd81BfIEM2COWvsOYcksNRnuqKpRhug"
     STRIPE_API_BASE: str = "https://api.stripe.com"
     
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
